7cae73e8aed0c081451d23979afddf680f141c78
trizzxxw <cmnascimento0702@gmail.com>
2026-08-29 21:17:42 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const http = require("http");
 const WebSocket = require("ws");
 const path = require("path");
 
-const app = express();
+const app = express();app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
